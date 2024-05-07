d2cc40bb147d1155a62862ad2fedef578511a973
BobbyLi123 <142700765+BobbyLi123@users.noreply.github.com>
2024-05-07 17:06:06 -0500

    Add files via upload

--- firing_rate_calculation.py
+++ firing_rate_calculation.py
@@ -1,4 +1,9 @@
 from pyopxclient import PyOPXClientAPI, OPX_ERROR_NOERROR
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import time
+from datetime import datetime
 
 SPIKE_TYPE = 1
 
@@ -15,6 +20,17 @@ def run():
 
     print ("Connected to OmniPlex Server\n")
 
+    num_groups = int(input("Enter the number of groups: "))
+    groups = {}
+    for i in range(num_groups):
+        units = input("Enter the units for group {}, separated by commas: ".format(i+1)).split(',')
+        units = [int(unit) for unit in units]
+        groups[i+1] = units
+
+    group_timestamps = {group: [] for group in range(1, num_groups+1)}
+    group_spike_densities = {group: [] for group in range(1, num_groups+1)}
+    group_channels = {group: set() for group in range(1, num_groups+1)}
+
     global_parameters = client.get_global_parameters()
     for source_id in global_parameters.source_ids:
         source_name, _, _, _ = client.get_source_info(source_id)
@@ -25,50 +41,46 @@ def run():
     # Include all spike sources
     source_ids = client.get_global_parameters().source_ids
 
-    # Initialize a dictionary to store the timestamps for each unit
-    unit_timestamps = {}
-
     # Define the time slot duration in milliseconds
     time_slot_duration = 1000
 
     try:
         while True:
             # Wait for new data for the duration of the time slot
-            client.opx_wait(time_slot_duration)
-            
+            # client.opx_wait(time_slot_duration)
             # Get a new batch of client data
+            time.sleep(time_slot_duration/1000.0)
+            
             new_data = client.get_new_data(timestamps_only=True)
             
             # Iterate over the new data
             for i in range(new_data.num_timestamps):
-                # If the data is a spike
-                # print(new_data.source_num_or_type[i])
-                # if new_data.source_num_or_type[i] == 6:
-                    # If the unit is not in the dictionary, add it
                 unit = new_data.unit[i]
-                if unit not in unit_timestamps:
-                    unit_timestamps[unit] = []
-                    # Add the timestamp to the list for this unit
-                unit_timestamps[unit].append(new_data.timestamp[i])
-                    # print ("Source: {}\tChannel: {}\tUnit: {}\tTS: {}".format(new_data.source_num_or_type[i], new_data.channel[i], new_data.unit[i], new_data.timestamp[i]))
+                for group, units in groups.items():
+                    if unit in units:
+                        # Add the timestamp to the list for this group
+                        group_timestamps[group].append(new_data.timestamp[i])
+                        group_channels[group].add(new_data.channel[i])
             
-            # Compute and print the spiking rate for each unit
-            for unit, timestamps in unit_timestamps.items():
+            # Compute and print the spike density for each group
+            for group, timestamps in group_timestamps.items():
                 # Compute the duration of the time slot in seconds
                 duration = time_slot_duration / 1000.0
-                # Compute the spiking rate
-                spiking_rate = len(timestamps) / duration
-                print ("Spiking rate for unit {}: {} spikes/sec".format(unit, spiking_rate))
+                # Compute the number of channels for this group
+                num_channels = max(len(group_channels[group]), 1)
+                # Compute the spike density
+                spike_density = len(timestamps) / (num_channels * duration)
+                # print("number of group_channels: ", num_channels)
+                print ("Spike density for group {}: {} Hz".format(group, spike_density))
+
+                group_spike_densities[group].append(spike_density)
 
             # Clear the timestamps for the next time slot
-            unit_timestamps = {}
+            group_timestamps = {group: [] for group in range(1, num_groups+1)}
+            # group_channels = {group: set() for group in range(1, num_groups+1)}
 
-            if client.last_result != OPX_ERROR_NOERROR:
-                print ("Error code: {}\n".format(client.last_result))
-                break
     except KeyboardInterrupt:
         print ("\nExiting...")
-
 
 if __name__ == '__main__':
     run()
